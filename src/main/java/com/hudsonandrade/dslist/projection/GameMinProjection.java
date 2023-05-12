45998d90246a678ba7c938d5a743b3a11a74d0eb
HudsonAnselmo38 <hjunior42@outlook.com>
2023-05-12 19:26:54 -0300

    Homolog ok

--- src/main/java/com/hudsonandrade/dslist/projection/GameMinProjection.java
+++ src/main/java/com/hudsonandrade/dslist/projection/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.hudsonandrade.dslist.projection;
 public interface GameMinProjection {
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
